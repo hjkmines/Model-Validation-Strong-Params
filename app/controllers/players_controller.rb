398d22c4f2c4174e7775e3a2288c6534bb9a4ed3
Tony Kim <hjkmines@gmail.com>
2020-07-23 15:19:42 -0600

    adds model validation

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,45 @@
 class PlayersController < ApplicationController
+  def index
+    if params[:search_params]
+      @players = Player.where("last_name LIKE ?", "%#{params[:search_params]}%")
+    else  
+    @players = Player.all 
+    render json: @players 
+  end 
+
+  def show 
+    @player = Player.find(params[:id])
+    render json: @player
+  end 
+
+  def create 
+    @player = Player.create(player_params)
+
+    render json: @player
+  end 
+
+  def update 
+    @player = Player.find(params[:id])
+    @player.update(
+      name: params[:id], 
+      number: params[:number], 
+      team_id: params[:team_id]
+    )
+
+    render json: @player 
+  end 
+
+  def destroy 
+    @players = Player.all 
+    @player = Player.find(params[:id])
+    @player.destroy 
+    render json: @players
+  end 
+
+  private
+
+  def player_params 
+    params.require(:player).permit(:name, :number, :team_id)
+  end 
+
 end
